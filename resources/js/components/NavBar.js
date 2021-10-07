2fc5d4e695eae8c0f26e426608a3554d5fbc9163
[folivora] <[info@folivora.design]>
2021-10-08 02:37:10 +0900

    ルーティング勉強

--- resources/js/components/NavBar.js
+++ resources/js/components/NavBar.js
@@ -5,6 +5,9 @@ function NavBar() {
     return (
         <nav>
             <ul className="nav">
+              <Link to="/">
+                <li className="ml-2">Top</li>
+              </Link>
               <Link to="/about">
                 <li className="ml-2">About</li>
               </Link>
--- resources/js/components/User.js
+++ resources/js/components/User.js
@@ -1,30 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function User() {
 
-    const users = [
-        {
-            id: 1,
-            name: 'John',
-            email: 'john@example.com'
-        },
-        {
-            id: 2,
-            name: 'Kevin',
-            email: 'kevin@test.com'
-        },
-        {
-            id: 3,
-            name: 'Joshua',
-            email: 'joshua@example.com'
-        }
-    ]
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        getUsers()
+    },[])
+
+    const getUsers = async () => {
+        const response = await axios.get('/api/user');
+        setUsers(response.data.users)
+    }
 
     return (
         <div>
             <h1>Userページ</h1>
             <ul>
-                {users.map((user) => <li key={user.id}>{user.name}</li>)}
+                {users.map((user) => <li key="{user.id}">{user.name}</li>)}
             </ul>
         </div>
     );
